3af9698d5d53a46da217b50d9b037dcc4ff62071
Konner <konsherman@fullsail.edu>
2015-01-26 21:54:47 -0500

    webrtc
    Messing around with webRTC

---
+++ webRTC/js/main.js
@@ -0,0 +1,20 @@
+(function(){
+	video = document.getElementById('video')
+	vendorUrl = window.URL || window.webkitURL;
+
+	navigator.getMedia = navigator.getUserMedia ||
+						 navigator.webkitGetUserMedia ||
+						 navigator.mozGetUserMedia ||
+						 navigator.msGetUserMedia;
+	navigator.getMedia({
+		video: true,
+		audio: false
+	},function(stream){
+		video.src = vendorUrl.createObjectURL(stream);
+		video.play();
+
+	}, function(error){
+
+	});
+
+})();
